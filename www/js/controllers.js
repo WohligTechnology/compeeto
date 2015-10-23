858734d31a339154fbf0c07d7dc02058ca9a83fd
Rohan Gada <gadarohan17@gmail.com>
2015-10-23 19:32:37 +0530

    controller added json and added page event-rate

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,56 @@
 angular.module('starter.controllers', [])
 
-.controller('LoginCtrl', function($scope, $ionicModal, $timeout) {
+.controller('LoginCtrl', function ($scope, $ionicModal, $timeout) {
 
 })
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
 })
 
-.controller('EventCtrl', function($scope) {
-
+.controller('EventCtrl', function ($scope) {
+    $scope.event = [{
+            id: 1,
+            name: "Event1",
+            date: "22/10/2015",
+            status: true
+},
+        {
+            id: 2
+            name: "Event2",
+            date: "23/10/2015",
+            status: true
+},
+        {
+            id: 3,
+            name: "Event3",
+            date: "24/10/2015",
+            status: true
+}];
 })
 
-.controller('EventInnerCtrl', function($scope, $stateParams) {
-  
+.controller('EventInnerCtrl', function ($scope, $stateParams) {
+    $scope.users = [{
+        id: 1,
+        name: 'Rohan',
+        ratingStatus: true,
+        rating: 6
+}, {
+        id: 2,
+        name: 'Sohan',
+        ratingStatus: true,
+        rating: 9
+}, {
+        id: 3,
+        name: 'Tushar',
+        ratingStatus: true,
+        rating: 10
+}, {
+        id: 4,
+        name: 'Amit',
+        ratingStatus: true,
+        rating: 9
+}];
 })
 
-.controller('RateCtrl', function($scope, $stateParams) {
-});
+.controller('RateCtrl', function ($scope, $stateParams) {});
