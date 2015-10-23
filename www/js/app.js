59f53a16f304df5bb9ec56dec1f7e40906e49b67
TusharSachde <tushar@wohlig.com>
2015-10-23 19:15:26 +0530

    Done

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,13 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
@@ -25,6 +16,13 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
+    .state('login', { // Mahesh
+    url: '/login',
+    abstract: true,
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  })
+
     .state('app', {
     url: '/app',
     abstract: true,
@@ -32,42 +30,36 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
+  .state('app.event', { // Amit
+    url: '/event',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/event.html',
+        controller: 'EventCtrl'
       }
     }
   })
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
+  .state('app.eventinner', { // Sohan
+    url: '/event/inner',
     views: {
       'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+        templateUrl: 'templates/event-inner.html',
+        controller: 'EventInnerCtrl'
       }
     }
-  });
+  })
+
+  .state('app.rate', { // Tushar
+    url: '/event/inner/rate',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/event-rate.html',
+        controller: 'RateCtrl'
+      }
+    }
+  })
+;
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/event');
 });
